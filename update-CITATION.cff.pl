a10cd91ba8b3879e6ea4bb6f7dd0def16cf39e17
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-04-06 09:20:08 +0800

    :bug: Fix bug about two 'name'
    Update .pre-commit-config.yaml
    Dump version

--- update-CITATION.cff.pl
+++ update-CITATION.cff.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/env -S perl -pi
 unless ( $#ARGV < 0 ) {
-    $name        = $1 if /^name = "([^"]*+)"/;
-    $description = $1 if /^description = "([^"]*+)"/;
-    $url         = $1 if /^Source = "([^"]*+)"/;
+    $name        = $1 if not defined $name and /^name = "([^"]*+)"/;
+    $description = $1
+      if not defined $description and /^description = "([^"]*+)"/;
+    $url = $1 if not defined $url and /^Source = "([^"]*+)"/;
     next;
 }
 s/(?<=^title: ")[^"]*/$name: $description/;
